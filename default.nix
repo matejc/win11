1218c607fc0325badbc978687305e5a194635452
Matej Cotman <matej@matejc.com>
2023-01-27 23:00:25 +0200

    update

--- default.nix
+++ default.nix
@@ -2,10 +2,21 @@
 let
   ovmf_fd = (pkgs.OVMF.override { secureBoot = true; tpmSupport = true; }).fd;
   qemu_kvm = pkgs.qemu_kvm.override { smbdSupport = true; };
+  looking-glass-client = pkgs.looking-glass-client.overrideDerivation (old: {
+    name = "looking-glass-client-B6";
+    src = pkgs.fetchFromGitHub {
+      owner = "gnif";
+      repo = "LookingGlass";
+      rev = "B6";
+      sha256 = "sha256-6vYbNmNJBCoU23nVculac24tHqH7F4AZVftIjL93WJU=";
+      fetchSubmodules = true;
+    };
+    buildInputs = old.buildInputs ++ [ pkgs.pipewire.dev pkgs.pulseaudio.dev pkgs.libsamplerate.dev ];
+  });
 in
 pkgs.stdenv.mkDerivation {
   name = "win11-env";
-  buildInputs = [ qemu_kvm pkgs.swtpm pkgs.looking-glass-client ];
+  buildInputs = [ qemu_kvm pkgs.swtpm looking-glass-client pkgs.spice-gtk ];
   shellHook = ''
     set -e
 
@@ -30,6 +41,12 @@ pkgs.stdenv.mkDerivation {
     test -f ./windows_11.qcow2 || qemu-img create -f qcow2 ./windows_11.qcow2 100G
 
     mkdir -p ./shared
+
+    if [ -d /dev/hugepages ]
+    then
+      test -d /dev/hugepages/win11 || sudo mkdir -p /dev/hugepages/win11
+      test -w /dev/hugepages/win11 || sudo chown -R $USER /dev/hugepages/win11
+    fi
 
     echo Run: ./run-win11.sh
   '';
